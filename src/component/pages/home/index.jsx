d9a2b12c28312a1935656133a6fcc0c32c54d897
Lý Thị Hà Giang <gianglth@dtp-education.com>
2022-03-31 16:24:44 +0700

    fix code

--- src/component/pages/home/index.jsx
+++ src/component/pages/home/index.jsx
@@ -1,12 +1,10 @@
 import { ListTag } from "../../atoms/listTag";
-import { Resume } from "../../atoms/molecules/resume";
 
 export const Home = ({ className = "", style }) => {
   return (
     <div className="p-home">
       <div className={`p-home ${className}`}>
-        {/* <ListTag/> */}
-        <Resume />
+        <ListTag />
       </div>
     </div>
   );
